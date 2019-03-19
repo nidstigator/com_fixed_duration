82a94177b9b0ddd1597b26fd4f8a71fbee122900
Nadim Atiya <nidstigator@nadims-imac.scis.ulster.ac.uk>
2019-03-19 15:50:33 +0000

    add rest of model results

--- plotting_code/plot_correlation.m
+++ plotting_code/plot_correlation.m
@@ -1,4 +1,6 @@
 % 
+% %call functions to test:
+close all;
 clearvars -except dynamics_and_results dynamics_and_results_2nddecision;
 
 global legends;
@@ -6,7 +8,7 @@ global titles;
 global export;
 global figures_path;
 
-figures_path = '../figures_output/Figure3/';
+figures_path = '../figures_output/csv/';
 
 legends = true;
 titles = false;
@@ -19,13 +21,14 @@ normalised = true;
 plot_response_time_uncertainty_correlation(dynamics_and_results)
 
 function plot_response_time_uncertainty_correlation(dynamics_and_results)
-
+global figures_path;
 [x_max] = ...
-    calculate_x_pattern(dynamics_and_results);
+    calculate_x_pattern(dynamics_and_results, true);
 
 
 [initiation_time_com_incorrect_mean_gather] = ...
-    calculate_mean_times(dynamics_and_results);
+    calculate_mean_times(dynamics_and_results, ...
+    true);
 
 corrcoef(x_max,initiation_time_com_incorrect_mean_gather)
 
@@ -35,11 +38,20 @@ V2 = normalise_custom(initiation_time_com_incorrect_mean_gather,max(initiation_t
 
 
 [~, c] = find(V1 == 0);
-V1(c) = NaN;
-V2(c) = NaN;
+V1(c) = [];
+V2(c) = [];
 
 mdl = fitlm(V1,V2);
 mdl.plot;
+
+data_file_name_csv=[figures_path 'uncertainty_IT_correlation' '.txt'];
+header = {'uncertainty,IT'};
+fig_1_data = [V1',V2'];
+fid = fopen(data_file_name_csv, 'w') ;
+fprintf(fid, '%s,', header{1,1:end-1}) ;
+fprintf(fid, '%s\n', header{1,end}) ;
+fclose(fid) ;
+dlmwrite(data_file_name_csv, fig_1_data(1:end,:), '-append') ;
 
 title(strcat('R² = ', num2str(sprintf('%.3f',mdl.Rsquared.Ordinary))))
 pubgraph(gcf,28,1,'w');
@@ -49,7 +61,7 @@ set(gcf, 'Position', [79 422 560 420])
 
 end
 function[maxes_hu_of_each_trial] = ...
-    calculate_x_pattern(dynamics_and_results)
+    calculate_x_pattern(dynamics_and_results, is_motor_correct)
 
 
 counter = 0;
@@ -58,6 +70,8 @@ trial_length =  dynamics_and_results(1).trial_length;
 
 
 for i = 1:size(dynamics_and_results,1)
+%     if(dynamics_and_results(i).is_motor_correct == is_motor_correct ...
+%             && dynamics_and_results(i).motor_decision_made)
     if(dynamics_and_results(i).motor_decision_made)
         counter = counter+1;
     end
@@ -67,6 +81,8 @@ y_mc_hu_gather = zeros(counter,trial_length);
 
 j=1;
 for i = 1:size(dynamics_and_results,1)
+%     if(dynamics_and_results(i).is_motor_correct == is_motor_correct...
+%             && dynamics_and_results(i).motor_decision_made)
     if(dynamics_and_results(i).motor_decision_made)
         y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
         j=j+1;
@@ -77,8 +93,33 @@ end
 % Max of each trial
 
 maxes_hu_of_each_trial =zeros(1,size(y_mc_hu_gather,1));
+% maxes_lu_of_each_trial =zeros(1,size(y_mc_hu_gather,1));
 for i = 1:size(y_mc_hu_gather,1)
     maxes_hu_of_each_trial(i) = max(y_mc_hu_gather(i,:));
+%     maxes_lu_of_each_trial(i) = max(y_mc_lu_gather(i,:));
+end
+
+
+%%% calculate x_pattern by peak
+max_hu = nanmean(maxes_hu_of_each_trial);
+std_hu = nanstd(maxes_hu_of_each_trial)/sqrt(size(maxes_hu_of_each_trial,2));
+% max_lu = nanmean(maxes_lu_of_each_trial);
+% std_lu = nanstd(maxes_lu_of_each_trial)/sqrt(size(maxes_lu_of_each_trial,2));
+% 
+% 
+% 
+% y_mc_hu_mean(1,:) = mean(y_mc_hu_gather);
+% y_mc_lu_mean(1,:) = mean(y_mc_lu_gather);
+
+%%% calculate x_pattern by integral (trapz)
+
+% Integral of each trial
+
+
+
+integral_hu_of_each_trial =zeros(1,size(y_mc_hu_gather,1));
+for i = 1:size(y_mc_hu_gather,1)
+    integral_hu_of_each_trial(i) = trapz(y_mc_hu_gather(i,:));
 end
 
 return;
@@ -86,7 +127,8 @@ return;
 end
 
 function[initiation_time_gather] = ...
-    calculate_mean_times(dynamics_and_results)
+    calculate_mean_times(dynamics_and_results, ...
+    is_motor_correct)
 
 
 
@@ -94,6 +136,8 @@ counter = 0;
 
 
 for i = 1:size(dynamics_and_results,1)
+%     if(dynamics_and_results(i).is_motor_correct ==...
+%             is_motor_correct && dynamics_and_results(i).motor_decision_made)
     if(dynamics_and_results(i).motor_decision_made)
         counter = counter+1;
     end
@@ -104,12 +148,17 @@ initiation_time_gather = zeros(1,counter);
 
 j=1;
 for i = 1:size(dynamics_and_results,1)
+%     if(dynamics_and_results(i).is_motor_correct ==...
+%             is_motor_correct && dynamics_and_results(i).motor_decision_made)
     if(dynamics_and_results(i).motor_decision_made)
-        initiation_time_gather(j)=dynamics_and_results(i).initiation_time -dynamics_and_results(i).stim_onset ;
+        initiation_time_gather(j)=dynamics_and_results(i).initiation_time ;
         j=j+1;
     end
 end
 
+
+initiation_time_mean = mean(initiation_time_gather);
+initiation_time_std = std(initiation_time_gather)/sqrt(size(initiation_time_gather,2));
 
 return;
 end
@@ -124,3 +173,12 @@ end
 return;
 end
 
+
+function [normalised_std] = normalise_std(std,max,min)
+
+normalised_std= std/(max-min);
+
+
+return;
+
+end
--- plotting_code/plot_figure_1.m
+++ plotting_code/plot_figure_1.m
@@ -41,142 +41,25 @@ plot_main_panel_modified(y_1,y_2,y_5,y_6,y_mc_hu,y_1_25,y_2_25,y_5_25,y_6_25,y_m
 is_motor_correct = true;
 is_motor_com = true;
 
+y_1_25 = [];
+y_2_25 = [];
+y_5_25 = [];
+y_6_25 = [];
+y_mc_hu_25 = [];
+
 [y_1, y_2, y_mc_hu, y_5, y_6] = ...
-    calculate_mean_activity(dynamics_and_results, 3.2, ...
+    calculate_mean_activity(dynamics_and_results, 6.4, ...
     is_motor_correct, is_motor_com);
-[y_1_25, y_2_25, y_mc_hu_25, y_5_25, y_6_25] = ...
-    calculate_mean_activity(dynamics_and_results, 25.6, ...
-    is_motor_correct, is_motor_com);
+
 plot_main_panel_modified(y_1,y_2,y_5,y_6,y_mc_hu,y_1_25,y_2_25,y_5_25,y_6_25,y_mc_hu_25,is_motor_com)
 
-plot_x_pattern(dynamics_and_results,normalised);
+com=false;
+plot_x_pattern(dynamics_and_results,com,normalised);
 
-plot_accuracy_vs_uncertainty(dynamics_and_results,normalised)
+com=true;
+plot_x_pattern(dynamics_and_results,com,normalised);
 
-function plot_accuracy_vs_uncertainty(dynamics_and_results,normalised)
-global titles;
-global export;
-global figures_path;
-global experiment_string;
-
-
-coherences = get_coherence_levels(dynamics_and_results);
-n_of_coherences= size(coherences,2);
-
-p_correct = zeros(1,n_of_coherences);
-x_correct_max = zeros(1,n_of_coherences);
-x_incorrect_max = zeros(1,n_of_coherences);
-
-x_correct_max_std = zeros(1,n_of_coherences);
-x_incorrect_max_std = zeros(1,n_of_coherences);
-
-for i=1:n_of_coherences
-   coherence= coherences(i);
-    
-    [noncom_correct_counter, noncom_incorrect_counter,...
-        com_correct_counter, com_incorrect_counter, ~, ~, ~] = ...
-        calculate_accuracies(dynamics_and_results, coherence);
-    
-    all_trials = noncom_correct_counter+ noncom_incorrect_counter ...
-        +com_correct_counter + com_incorrect_counter;
-    [noncom_correct_counter, ~,...
-        com_correct_counter, ~, ~, ~, ~] = ...
-        calculate_accuracies(dynamics_and_results, coherence);
-    
-    p_correct(i) = (noncom_correct_counter+com_correct_counter)/all_trials;
-    [x_correct_max(i), x_correct_max_std(i), ~,~] = ...
-        calculate_x_pattern(dynamics_and_results, coherence, true);
-    
-    [x_incorrect_max(i), x_incorrect_max_std(i), ~,~] = ...
-        calculate_x_pattern(dynamics_and_results, coherence, false);
-    
-end
-
-confidence_max(1,:)= nanmean([x_correct_max; x_incorrect_max]);
-if(normalised)
-    confidence_max = normalise_custom(confidence_max, max(confidence_max), min(confidence_max));
-end
-
-%%%%%%%%Plot7: Accuracy v.s Uncertainty using peak.
-
-title_string = 'Accuracy v.s uncertainty';
-x_string = 'Uncertainty \upsilon (%)';
-
-
-% [fitObject, gof] = fit(confidence_max',p_correct','poly1');
-
-confidence_max(:,6) = [];
-p_correct(:,6) = [];
-
-% Set up fittype and options.
-ft = fittype( 'smoothingspline' );
-opts = fitoptions( 'Method', 'SmoothingSpline' );
-opts.Normalize = 'on';
-opts.SmoothingParam = 0.914519350220469;
-
-% Fit model to data.
-[fitObject, ~] = fit( confidence_max', p_correct', ft, opts );
-
-%%% extract data from fitted curve
-figuretemp = figure;
-plot(fitObject);
-h = findobj(figuretemp,'Type','line');
-x=get(h,'Xdata');
-y=get(h,'Ydata');
-close;
-%%%%%%%
-
-
-figure7=figure;
-
-plot(x,y,'LineWidth', 2,'Color',[0 0 0]);
-hold on;
-
-scatter(confidence_max,p_correct,'o','LineWidth',4,'MarkerEdgeColor',[0 0 0],'MarkerFaceColor', [0 0 0]);
-yticklabels({'60','65','70','75','80','85','90','100'})
-ylim([0.6,1.01])
-xlim([0.79, 1.0])
-xticklabels({'80','85','90','95','100'});
-xticks([0.8, 0.85, 0.9, 0.95, 1])
-ylabel('Accuracy p (%)');
-xlabel(x_string);
-pubgraph(figure7,18,4,'w')
-
-
-data_file_name_csv_1=[figures_path 'uncertainty_vs_accuracy_spline' experiment_string '.txt'];
-data_file_name_csv_2=[figures_path 'uncertainty_vs_accuracy' experiment_string '.txt'];
-header_1 = {'x_fit,y_fit'};
-header_2 = {'x_real,y_real'};
-
-fig_1_data_1 = [x',y'];
-fig_1_data_2 = [confidence_max',p_correct'];
-
-
-fid = fopen(data_file_name_csv_1, 'w') ;
-fprintf(fid, '%s,', header_1{1,1:end-1}) ;
-fprintf(fid, '%s\n', header_1{1,end}) ;
-fclose(fid) ;
-dlmwrite(data_file_name_csv_1, fig_1_data_1(1:end,:), '-append') ;
-
-fid = fopen(data_file_name_csv_2, 'w') ;
-fprintf(fid, '%s,', header_2{1,1:end-1}) ;
-fprintf(fid, '%s\n', header_2{1,end}) ;
-fclose(fid) ;
-dlmwrite(data_file_name_csv_2, fig_1_data_2(1:end,:), '-append') ;
-
-if(titles)
-    title(title_string);
-end
-
-if(export)
-export_path = [figures_path  'accuracyvsuncertainty'];
-export_fig (export_path, '-nofontswap', '-linecaps','-png', '-transparent','-m10','-q101', '-cmyk','-painters');
-savefig(export_path);
-end
-%%%%%%%%
-
-end
-function plot_x_pattern(dynamics_and_results,normalised)
+function plot_x_pattern(dynamics_and_results, com ,normalised)
 
 global legends;
 global titles;
@@ -201,11 +84,11 @@ for i=1:n_of_coherences
     coherence = coherences(i);
     [x_correct_max(i), x_correct_max_std(i),...
         x_correct_integral(i), x_correct_integral_std(i)] = ...
-        calculate_x_pattern(dynamics_and_results, coherence, true);
+        calculate_x_pattern(dynamics_and_results, coherence, true, com);
         
     [x_incorrect_max(i), x_incorrect_max_std(i),...
         x_incorrect_integral(i),x_incorrect_integral_std(i)] = ...
-        calculate_x_pattern(dynamics_and_results, coherence, false);
+        calculate_x_pattern(dynamics_and_results, coherence, false, com);
 end
 
 %%%% HU stuff:
@@ -261,7 +144,15 @@ pubgraph(figure1,18,4,'w')
 ylim([-1,110]);
 
 data_file_name_csv=[figures_path 'uncertainty_vs_coherence' experiment_string '.txt'];
+if(com)
+    data_file_name_csv=[figures_path 'uncertainty_vs_coherence_com' experiment_string '.txt'];
+    x_correct_max(6) = [];
+    x_correct_max_std(6) = [];
+    x_incorrect_max(6) = [];
+    x_incorrect_max_std(6) = [];
+end
 header = {'x_correct,x_correct_sem,x_error,x_error_sem'};
+
 
 fig_1_data = [x_correct_max'*100,x_correct_max_std'*100,x_incorrect_max'*100,x_incorrect_max_std'*100,];
 
@@ -301,7 +192,6 @@ large_right = 'c`=25.6%, right';
 
 small_left= 'c`=3.2%, left';
 small_right = 'c`=3.2%, right';
-
 
 large = 'c`=51.2%';
 small = 'c`=3.2%';
@@ -403,7 +293,13 @@ trial_length =  dynamics_and_results(1).trial_length;
 
 for i = 1:size(dynamics_and_results,1)
     if(dynamics_and_results(i).coherence_level == coherence && dynamics_and_results(i).motor_decision_made && dynamics_and_results(i).is_motor_correct == is_motor_correct && dynamics_and_results(i).is_motor_com == is_motor_com)
-        counter = counter+1;
+        if(is_motor_com)
+            if(check_com_advanced_condition(dynamics_and_results,i))
+                counter = counter+1;
+            end
+        else
+            counter = counter+1;
+        end
     end
 end
 
@@ -417,12 +313,24 @@ y_6_gather = zeros(counter,trial_length);
 j=1;
 for i = 1:size(dynamics_and_results,1)
     if(dynamics_and_results(i).coherence_level == coherence && dynamics_and_results(i).motor_decision_made && dynamics_and_results(i).is_motor_correct == is_motor_correct && dynamics_and_results(i).is_motor_com == is_motor_com)
-        y_1_gather(j,:)=dynamics_and_results(i).y_1;
-        y_2_gather(j,:)=dynamics_and_results(i).y_2;
-        y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
-        y_5_gather(j,:)=dynamics_and_results(i).y_5;
-        y_6_gather(j,:)=dynamics_and_results(i).y_6;
-        j=j+1;
+        if(is_motor_com)
+            if(check_com_advanced_condition(dynamics_and_results,i))
+                y_1_gather(j,:)=dynamics_and_results(i).y_1;
+                y_2_gather(j,:)=dynamics_and_results(i).y_2;
+                y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
+                y_5_gather(j,:)=dynamics_and_results(i).y_5;
+                y_6_gather(j,:)=dynamics_and_results(i).y_6;
+                j=j+1;
+            end
+        else
+            y_1_gather(j,:)=dynamics_and_results(i).y_1;
+            y_2_gather(j,:)=dynamics_and_results(i).y_2;
+            y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
+            y_5_gather(j,:)=dynamics_and_results(i).y_5;
+            y_6_gather(j,:)=dynamics_and_results(i).y_6;
+            j=j+1;
+        end
+
     end
 end
 
@@ -451,7 +359,7 @@ return
 end
 
 function[max_hu, std_hu, integral_hu, integral_hu_std] = ...
-    calculate_x_pattern(dynamics_and_results, coherence, is_motor_correct)
+    calculate_x_pattern(dynamics_and_results, coherence, is_motor_correct, com)
 
 coherences = get_coherence_levels(dynamics_and_results);
 
@@ -463,26 +371,60 @@ counter = 0;
 
 trial_length =  dynamics_and_results(1).trial_length;
 
-
-for i = 1:size(dynamics_and_results,1)
-    if(dynamics_and_results(i).coherence_level == coherence &&...
-            dynamics_and_results(i).is_motor_correct == is_motor_correct...
-            && dynamics_and_results(i).motor_decision_made)
-        counter = counter+1;
+if(com)
+    for i = 1:size(dynamics_and_results,1)
+        if(dynamics_and_results(i).coherence_level == coherence &&...
+                dynamics_and_results(i).motor_decision_made)
+            if(is_motor_correct)
+                if(check_com_advanced_condition(dynamics_and_results,i))     
+                counter = counter+1;
+                end
+            else
+                counter = counter+1;
+            end
+        end
+    end
+else
+    for i = 1:size(dynamics_and_results,1)
+        if(dynamics_and_results(i).coherence_level == coherence &&...
+                dynamics_and_results(i).is_motor_correct == is_motor_correct...
+                && dynamics_and_results(i).motor_decision_made)
+            counter = counter+1;
+        end
     end
 end
+
+
 
 y_mc_hu_gather = zeros(counter,trial_length);
-
-j=1;
-for i = 1:size(dynamics_and_results,1)
-    if(dynamics_and_results(i).coherence_level == coherence &&...
-            dynamics_and_results(i).is_motor_correct == is_motor_correct...
-            && dynamics_and_results(i).motor_decision_made)
-        y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
-        j=j+1;
+if(com)
+    j=1;
+    for i = 1:size(dynamics_and_results,1)
+        if(dynamics_and_results(i).coherence_level == coherence ...
+                && dynamics_and_results(i).motor_decision_made)
+            if(is_motor_correct)
+                if(check_com_advanced_condition(dynamics_and_results,i))
+                    y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
+                    j=j+1;
+                end
+            else
+                y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
+                j=j+1;
+            end
+        end
+    end
+else
+    j=1;
+    for i = 1:size(dynamics_and_results,1)
+        if(dynamics_and_results(i).coherence_level == coherence &&...
+                dynamics_and_results(i).is_motor_correct == is_motor_correct...
+                && dynamics_and_results(i).motor_decision_made)
+            y_mc_hu_gather(j,:)=dynamics_and_results(i).y_mc_hu;
+            j=j+1;
+        end
     end
 end
+
 
 
 % Max of each trial
@@ -531,7 +473,7 @@ return;
 end
 
 function[noncom_correct_counter, noncom_incorrect_counter,...
-    com_correct_counter, com_incorrect_counter, com_late_correct_counter, com_late_incorrect_counter,nondecision_counter] = ...
+    com_correct_counter, com_incorrect_counter,nondecision_counter] = ...
     calculate_accuracies(dynamics_and_results, coherence)
 
 coherences = get_coherence_levels(dynamics_and_results);
@@ -544,31 +486,79 @@ noncom_correct_counter = 0;
 noncom_incorrect_counter = 0;
 com_correct_counter = 0;
 com_incorrect_counter = 0;
-
 nondecision_counter = 0;
 
-com_late_correct_counter = 0;
-com_late_incorrect_counter = 0;
 
 for i = 1:size(dynamics_and_results,1)
     if(coherence==dynamics_and_results(i).coherence_level)
         if(dynamics_and_results(i).motor_decision_made)
             if(dynamics_and_results(i).is_motor_correct)
                 if(dynamics_and_results(i).is_motor_com)
-                    com_correct_counter = com_correct_counter+1;
+                    if(check_com_advanced_condition(dynamics_and_results, i))
+                        com_correct_counter = com_correct_counter+1;
+                    end
                 else
                     noncom_correct_counter = noncom_correct_counter+1;
                 end
             elseif(dynamics_and_results(i).is_motor_com)
-                com_incorrect_counter = com_incorrect_counter+1;
+                if(check_com_advanced_condition(dynamics_and_results, i))
+                    com_incorrect_counter = com_incorrect_counter+1;
+                end
             else
                 noncom_incorrect_counter = noncom_incorrect_counter+1;
             end
         else
             nondecision_counter = nondecision_counter+1;
         end
+        
     end
 end
 return;
+end
+
+function [itiscom] = check_com_advanced_condition(dynamics_and_results, index)
+
+itiscom = false;
+reached = find(dynamics_and_results(index).y_5>=17.4,1);
+if(dynamics_and_results(index).is_motor_correct)
+    reached = find(dynamics_and_results(index).y_6>=17.4,1);
+end
+
+x_traj = dynamics_and_results(index).y_6-dynamics_and_results(index).y_5;
+
+if(dynamics_and_results(index).is_motor_correct)
+    x_traj = dynamics_and_results(index).y_5-dynamics_and_results(index).y_6;
+end
+
+smoothed_trajectory = filter(ones(1,200)/200,1,x_traj);
+delta_of_trajectory = diff(sign(smoothed_trajectory));
+
+points_of_change = find(delta_of_trajectory);
+
+if(size(points_of_change,2)<2)
+    return;
+end
+
+for i=size(points_of_change,2):-1:2
+    if(max(smoothed_trajectory(points_of_change(i-1):points_of_change(i)))>2)
+        sign_of_response = sign(x_traj(reached));
+        sign_of_max_point_in_bump =  sign(max(smoothed_trajectory(points_of_change(i-1):points_of_change(i))));
+        
+        
+        if(sign_of_response *  sign_of_max_point_in_bump == -1)
+            if(max(abs(smoothed_trajectory(points_of_change(i-1):points_of_change(i))))>2)
+%                 figure;
+%                 plot(smoothed_trajectory)
+                itiscom=true;
+                return;
+            end
+
+        end
+
+    end
+    
+end
+
+return
 
 end
