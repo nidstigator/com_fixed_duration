3aa9fda3645781cb0f04b9243fd103448005aa43
Arkady Zgonnikov <azgonnikov@tudelft.net>
2019-09-30 13:22:25 +0200

    Analysis of new data added

--- data_preprocessing/fix_session_no.py
+++ data_preprocessing/fix_session_no.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 
 def fix_headers(directory):
-# delete 'session_no' from the heading in each file
+# delete 'session_no' from the header in each file
     for i, file_name in enumerate(os.listdir(directory)):
         file_path = os.path.join(directory, file_name)
         with open(file_path) as f:
@@ -15,7 +15,6 @@ def fix_headers(directory):
 
 def fix_session_no(directory, session_no = 1):
 # insert session_no column to dynamics log files
-# applies to all dynamics logs, as session number mistakenly was not recorded 
     for i, file_name in enumerate(os.listdir(directory)):
         file_path = os.path.join(directory, file_name)
         dynamics = pd.read_csv(file_path, sep='\t')
@@ -31,13 +30,11 @@ def fix_choices_session_no(choices_directory, file_name, session_no = 1):
     choices.set_index(['subj_id', 'session_no', 'block_no', 'trial_no'], inplace=True, drop=True)
     choices.to_csv(os.path.join(file_path), sep='\t')
 
-path = 'C:/Users/azgonnikov/Google Drive/data/CoM_fixed_duration/dresden_raw'
-
-dynamics_directory = os.path.join(path, 'dynamics')
-choices_directory = os.path.join(path, 'choices')
+path = 'C:/Users/azgonnikov/Google Drive/data/CoM_fixed_duration/'
 
 # First, fix 269's first session choice log, which mistakenly doesn't contain session_no column
 # no other problems in choice files
+#choices_directory = os.path.join(path, 'choices')
 #choices_file = '269_Sep_01_2016_17_07_18_choices.txt'
 #fix_choices_session_no(choices_directory, choices_file)
 
@@ -45,5 +42,7 @@ choices_directory = os.path.join(path, 'choices')
 # was not actually recorded to dynamics log files, although the headers contain 'session_no'. 
 # To fix this, we first remove 'session_no' from the header of each dynamics file, and then
 # insert new column to these files, one session at a time (change session_no from 1 to 4)
-fix_headers(dynamics_directory)
-fix_session_no(dynamics_directory, session_no = 1)
+for session in range(1,5):    
+    dynamics_directory = os.path.join(path, 'dresden_raw/dynamics_corrupt/session_%i' % (session))
+    fix_headers(dynamics_directory)
+    fix_session_no(dynamics_directory, session_no = session)
